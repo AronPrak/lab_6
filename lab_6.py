a5f8e478e589638fe695901b98d6664024fa0397
aronprakash03@gmail.com <aronprakash03@gmail.com>
2023-10-24 18:00:10 -0400

    first commit

---
+++ lab_6.py
@@ -0,0 +1,16 @@
+#Aron Prakash
+def encoder(value):
+    new_value = ''
+    for i in password:
+        new_value += str(int(i) + 3)
+    return new_value
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
