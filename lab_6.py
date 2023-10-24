47e7ce2ba6ea549316051ab9e32219e402b5c603
aronprakash03@gmail.com <aronprakash03@gmail.com>
2023-10-24 18:44:46 -0400

    fixed the main function

--- lab_6.py
+++ lab_6.py
@@ -5,6 +5,9 @@ def encoder(value):
     for i in str_value:
         new_value += str(int(i) + 3)
     return new_value
+def decoder(encoded):
+    pass
+
 def menu():
     print("Menu")
     print('-------------')
@@ -22,9 +25,10 @@ def main():
         if option == 1:
                 passcode = int(input("Please enter your password to encode:"))
                 print("Your password has been encoded and stored!")
-        elif option == 2:
                 new_value = encoder(passcode)
-                print(f"The encoded password is {new_value}, and the original password is {passcode}.")
+        elif option == 2:
+                decoded_passcode = decoder(new_value)
+                print(f"The encoded password is {new_value}, and the original password is {decoded_passcode}.")
         elif option == 3:
             break
 
